fb45ae54311f8466181285ded9855509d772ab70
Daniel <d.cunniffe.ie@gmail.com>
2019-06-08 21:12:22 +0100

    Fixing some initial typos

---
+++ employee-management-system-server/src/main/java/com/danielcgithub/employeemanagementsystemserver/EmployeeManagementSystemServerApplication.java
@@ -0,0 +1,13 @@
+package com.danielcgithub.employeemanagementsystemserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementSystemServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagementSystemServerApplication.class, args);
+	}
+
+}
--- employee-management-system-server/src/test/java/com/danielcgithub/employeemanagementsystemserver/EmployeeManagementSystemServerApplicationTests.java
+++ employee-management-system-server/src/test/java/com/danielcgithub/employeemanagementsystemserver/EmployeeManagementSystemServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.danielcgithub.employeemanagementsystemclient;
+package com.danielcgithub.employeemanagementsystemserver;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class EmployeeManagementSystemClientApplicationTests {
+public class EmployeeManagementSystemServerApplicationTests {
 
 	@Test
 	public void contextLoads() {
