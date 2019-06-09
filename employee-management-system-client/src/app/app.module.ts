c9cc6976805e047b1f486e7507f8c34c801cf4cf
Daniel <d.cunniffe.ie@gmail.com>
2019-06-09 21:16:09 +0100

    Creating emp service, create emp comp., emp details comp., emp list comp.

--- employee-management-system-client/src/app/app.module.ts
+++ employee-management-system-client/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CreateEmployeeComponent } from './create-employee/create-employee.component';
+import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateEmployeeComponent,
+    EmployeeDetailsComponent,
+    EmployeeListComponent
   ],
   imports: [
     BrowserModule
