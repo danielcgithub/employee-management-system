7734557057ba0b99ccdb39381d8fb3b4b577fed7
Daniel <d.cunniffe.ie@gmail.com>
2019-06-11 21:55:56 +0100

    Adding initial test (not working - pushing up for access on another pc)

--- employee-management-system-server/src/test/java/com/danielcgithub/employeemanagementsystemserver/EmployeeManagementSystemServerApplicationTests.java
+++ employee-management-system-server/src/test/java/com/danielcgithub/employeemanagementsystemserver/EmployeeManagementSystemServerApplicationTests.java
@@ -1,16 +1,31 @@
 package com.danielcgithub.employeemanagementsystemserver;
 
+import java.util.List;
+
+import com.github.tomakehurst.wiremock.client.WireMock;
+
+import org.assertj.core.api.BDDAssertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureWireMock(port = 9000)
 public class EmployeeManagementSystemServerApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	public void test_should_return_all_employees() {
+		WireMock.stubFor(
+				WireMock.get(WireMock.urlEqualTo("/employees")).willReturn(WireMock.aResponse().withStatus(201)));
+
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<List> responseEntity = restTemplate.getForEntity("http://localhost:9000/employees", List.class);
+		BDDAssertions.assertThat(responseEntity.getStatusCode()).isEqualTo(201);
 	}
 
 }
