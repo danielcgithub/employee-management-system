a437a066649014a2e0fc6102b3d7cde83ff83af8
danielcgithub <d.cunniffe.ie@gmail.com>
2019-06-12 17:07:30 +0100

    Including in mem db, fixing test

--- employee-management-system-server/src/test/java/com/danielcgithub/employeemanagementsystemserver/EmployeeManagementSystemServerApplicationTests.java
+++ employee-management-system-server/src/test/java/com/danielcgithub/employeemanagementsystemserver/EmployeeManagementSystemServerApplicationTests.java
@@ -7,6 +7,8 @@ import com.github.tomakehurst.wiremock.client.WireMock;
 import org.assertj.core.api.BDDAssertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.http.ResponseEntity;
@@ -16,16 +18,18 @@ import org.springframework.web.client.RestTemplate;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureWireMock(port = 9000)
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 public class EmployeeManagementSystemServerApplicationTests {
 
 	@Test
 	public void test_should_return_all_employees() {
-		WireMock.stubFor(
-				WireMock.get(WireMock.urlEqualTo("/employees")).willReturn(WireMock.aResponse().withStatus(201)));
+		WireMock.stubFor(WireMock.get(WireMock.urlEqualTo("/api/v1/employees"))
+				.willReturn(WireMock.aResponse().withStatus(201)));
 
 		RestTemplate restTemplate = new RestTemplate();
-		ResponseEntity<List> responseEntity = restTemplate.getForEntity("http://localhost:9000/employees", List.class);
-		BDDAssertions.assertThat(responseEntity.getStatusCode()).isEqualTo(201);
+		ResponseEntity<List> responseEntity = restTemplate.getForEntity("http://localhost:9000/api/v1/employees",
+				List.class);
+		BDDAssertions.assertThat(responseEntity.getStatusCodeValue()).isEqualTo(201);
 	}
 
 }
